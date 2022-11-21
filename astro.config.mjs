94eab859288fa327979043b38c7e61748b964cab
Jon Coleman <jcoleman1388@gmail.com>
2022-11-21 09:00:22 -0500

    changing to files instead of directories did not change nav active class behavior

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,3 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  build: {
-    // Example: Generate `page.html` instead of `page/index.html` during build.
-    format: 'file',
-  },
-});
+export default defineConfig({});
